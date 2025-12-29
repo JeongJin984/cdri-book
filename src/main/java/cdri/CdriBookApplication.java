c4c7a7ee92ae7cda058a66dde088b9520d09fdcc
jay <jay@eximbay.com>
2025-12-29 10:34:05 +0900

    first commit

---
+++ src/main/java/cdri/CdriBookApplication.java
@@ -0,0 +1,13 @@
+package cdri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdriBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdriBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cdri/CdriBookApplicationTests.java
@@ -0,0 +1,13 @@
+package cdri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdriBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
